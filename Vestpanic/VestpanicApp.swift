e4827793303b8ccb4efac66261eb9ca1a9aa3f4b
Zakki Mudhoffar <zakki.mudhoffar@gmail.com>
2022-06-24 17:03:49 +0700

    Initial Commit

---
+++ Vestpanic/VestpanicApp.swift
@@ -0,0 +1,17 @@
+//
+//  VestpanicApp.swift
+//  Vestpanic
+//
+//  Created by Zakki Mudhoffar on 24/06/22.
+//
+
+import SwiftUI
+
+@main
+struct VestpanicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
